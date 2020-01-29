76a5df8037cb94eadad63e2d7d31aebf450d94c3
root <root@bmr.idrive.com>
2020-01-29 07:52:03 -0500

    zfs dataset mount issue

--- idrivebmrserver/server.cpp
+++ idrivebmrserver/server.cpp
@@ -724,6 +724,10 @@ void BackupServer::testSnapshotAvailability(IDatabase *db)
 		"SELECT value FROM settings_db.settings WHERE key=? AND clientid=0");
 	Server->Log("Testing if backup destination can handle subvolumes and snapshots...", LL_DEBUG);
 
+
+        IDatabase *db_settings = NULL;
+        db_settings = Server->getDatabase(Server->getThreadID(), IDRIVEBMRDB_SETTINGS);
+
 	std::string snapshot_helper_cmd=Server->getServerParameter("snapshot_helper");
 	if(!snapshot_helper_cmd.empty())
 	{
@@ -757,14 +761,24 @@ void BackupServer::testSnapshotAvailability(IDatabase *db)
 			}
 			else
 			{
-				Server->Log("Copy on write mode is disabled, because the filesystem does not support it anymore.", LL_ERROR);
-				db->BeginWriteTransaction();
-				db->Write("DELETE FROM settings_db.settings WHERE key='cow_mode' AND clientid=0");
-				db->Write("INSERT INTO settings_db.settings (key, value, clientid) VALUES ('cow_mode', 'false', 0)");
-				db->EndTransaction();
+                               //ID157463562-LocalBackupFailure
+                               if (!db_settings)
+                               {
+                                       Server->Log("Couldn't open backup server database. Exiting. Expecting database at \""
+                                               + Server->getServerWorkingDir() + os_file_sep() + "idrivebmr" + os_file_sep() + "settings.db\"", LL_ERROR);
+                                       exit(1);
+                               }
+                               else
+                               {
+                                       db_settings->BeginWriteTransaction();
+                                       db_settings->Write("UPDATE generalKeyValueSettings SET data='BAD' WHERE name='zfs_dataset_status'");
+                                       db_settings->EndTransaction();
+                                       Server->Log("Shutting down the server as zfs is corrupted!", LL_ERROR);
+                                       Server->Log("Contact support for further assistance.", LL_ERROR);
+                                       Server->destroy(db_settings);
+                                       exit(1);
+                               }
 
-				image_snapshots_enabled = false;
-				file_snapshots_enabled = false;
 			}
 		}
 	}
@@ -777,6 +791,20 @@ void BackupServer::testSnapshotAvailability(IDatabase *db)
 		db->Write("INSERT INTO settings_db.settings (key, value, clientid) VALUES ('cow_mode', 'true', 0)");
 		db->EndTransaction();
 	}
+
+
+	if (!db_settings)
+       	{
+               Server->Log("Couldn't open backup server database. Exiting. Expecting database at \""
+                       + Server->getServerWorkingDir() + os_file_sep() + "idrivebmr" + os_file_sep() + "settings.db\"", LL_ERROR);
+               exit(1);
+       	}
+       	else
+       	{
+               db_settings->Write("UPDATE generalKeyValueSettings SET data='GOOD' WHERE name='zfs_dataset_status'");
+               Server->destroy(db_settings);
+       	}
+
 
 	if(image_snapshots_enabled || file_snapshots_enabled)
 	{
